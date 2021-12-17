2f8db27411c9c700ebfccb454597b70031500fb0
Felicity2724 <47048748+Felicity2724@users.noreply.github.com>
2021-12-17 22:58:34 +0800

    update 2

--- timeline on foodaid/js/timeline.js
+++ timeline on foodaid/js/timeline.js
@@ -23,7 +23,8 @@
     "x": "1",
     "y": 3,
     "text":"[bold]2020 Mar 4[/]\n" + 'Short-term Food Assistance Service by Hong Kong Sheng Kung Hui Welfare Council',
-    "center": "bottom"
+    "center": "bottom",
+    "url": "https://www.facebook.com/permalink.php?story_fbid=2828654993847292&id=141224685923683"
     }, {
     "x": "2",
     "y": 3,
@@ -43,12 +44,14 @@
     "x": "4",
     "y": 2,
     "text": "[bold]2020 Sep 15[/]\nJockey Club Food Assistance Programme.",
-    "center": "bottom"
+    "center": "bottom",
+    "url": "https://corporate.hkjc.com/corporate/corporate-news/english/2020-09/news_2020091501406.aspx"
     }, {
     "x": "3",
     "y": 2,
     "text": "[bold]2020 Sep 16[/]\nNew security measures are put in place to prevent further accidents.",
-    "center": "bottom"
+    "center": "bottom",
+    "url": "https://fb.watch/9FJh-R_KpU/"
     }, {
     "x": "2",
     "y": 2,
@@ -76,11 +79,14 @@
         "x": "3",
         "y": 1,
         "text": "[bold]2021 Jun 1[/]\nShort-term Food Assistance Service by St. James’Settlement.",
-        "center": "top"
+        "center": "top",
+        "url": "https://drive.google.com/file/d/1lV_28JsD7Kesjn1scY7hfo79AkXA1xqO/view?fbclid=IwAR0XSC2g3aJwLYtWp5to9j7ILTTK9LFr_NFyez3P_EDqwy_Nlk2_1lc-Dvw"
         }
     
     ];
 
+    
+    
     // Create axes
     var xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
     xAxis.dataFields.category = "x";
@@ -96,6 +102,8 @@
     }, {
     "x": "4"
     }];
+
+
 
     var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
     yAxis.min = 0;
@@ -117,6 +125,18 @@
     series.fill = am4core.color("#424242");
     series.fontFamily="Roboto Condensed"
     series.tensionX = 0.4;
+
+    // Make it interactive
+    //labelBullet.label.interactionsEnabled = true;
+    //labelBullet.label.keepTargetHover = true;
+
+    // Set up tooltips
+    series.tooltip.label.interactionsEnabled = true;
+    series.tooltip.keepTargetHover = true;
+    //series.columns.template.tooltipHTML = '<b>{category}</b><br><a href="https://en.wikipedia.org/wiki/{category.urlEncode()}">More info</a>';
+
+    //polygonSeries.calculateVisualCenter = true;
+    //polygonSeries.mapPolygons.template.tooltipPosition = "fixed";
 
     var bullet = series.bullets.push(new am4charts.CircleBullet());
     bullet.circle.radius = 5;
@@ -143,8 +163,9 @@
     labelBullet.states.create("hover").properties.scale = 1.2;
     labelBullet.label.states.create("hover").properties.fill = am4core.color("#EBB471");
 
-
     labelBullet.label.propertyFields.verticalCenter = "center";
+
+    labelBullet.label.propertyFields.url = "url";
 
     var title = chart.titles.create();
     title.text = "Timeline for food-aid in Hong Kong during COVID-19";
